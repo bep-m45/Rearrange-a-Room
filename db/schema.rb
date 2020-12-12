321e6d9a294c8d72ff72bb80a0c0bf2e159ae8e8
EC2 Default User <ec2-user@ip-172-31-41-214.ap-northeast-1.compute.internal>
2020-12-12 05:03:08 +0000

    [Fix]envelonments

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2020_11_28_071440) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "category_name"
+    t.string "room_layout_name"
+    t.string "room_image_name"
+    t.string "room_genre_name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
   create_table "chats", force: :cascade do |t|
     t.integer "member_id"
     t.datetime "created_at", null: false
@@ -38,13 +50,19 @@ ActiveRecord::Schema.define(version: 2020_11_28_071440) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "member_id", null: false
-    t.integer "room_id", null: false
+    t.integer "member_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["member_id", "room_id"], name: "index_favorites_on_member_id_and_room_id", unique: true
     t.index ["member_id"], name: "index_favorites_on_member_id"
     t.index ["room_id"], name: "index_favorites_on_room_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", force: :cascade do |t|
@@ -114,12 +132,10 @@ ActiveRecord::Schema.define(version: 2020_11_28_071440) do
 
   create_table "rooms", force: :cascade do |t|
     t.integer "member_id"
+    t.integer "category"
     t.string "size"
     t.text "production"
     t.string "image_id"
-    t.integer "room_layout_id"
-    t.integer "room_image_id"
-    t.integer "room_genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
